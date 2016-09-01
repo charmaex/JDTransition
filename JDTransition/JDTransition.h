b52d6e7a23eb7e2174ef032ddecb0ba4072b483e
Jan Dammshäuser <jandammshaeuser@me.com>
2016-09-01 19:17:05 +0200

    Initial Commit

---
+++ JDTransition/JDTransition.h
@@ -0,0 +1,19 @@
+//
+//  JDTransition.h
+//  JDTransition
+//
+//  Created by Jan Dammshäuser on 01.09.16.
+//  Copyright © 2016 Jan Dammshäuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JDTransition.
+FOUNDATION_EXPORT double JDTransitionVersionNumber;
+
+//! Project version string for JDTransition.
+FOUNDATION_EXPORT const unsigned char JDTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDTransition/PublicHeader.h>
+
+
